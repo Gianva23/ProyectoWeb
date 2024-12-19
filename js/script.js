0d58a004c1adad2958901fcf32939c681300858f
Gian <gianlucavanella99@gmail.com>
2024-12-19 10:03:47 -0300

    lee productos de api

--- js/script.js
+++ js/script.js
@@ -1,3 +1,54 @@
-fetch('https://dummyjson.com/products')
-.then(response => response.json())
-.then(data => console.log(data));
+document.addEventListener("DOMContentLoaded", () => 
+    {
+    const container = document.getElementById("contenedor");
+  
+    function fetchProductos() 
+    {
+      fetch("https://dummyjson.com/products?limit=15")
+        .then((response) => response.json())
+        .then((data) => {
+          const productos = data.products;
+  
+          container.innerHTML = ""; //limpio contenedor
+  
+          productos.forEach((product) => 
+            {
+            const cardDiv = document.createElement("div");
+            cardDiv.className = "col-md-15"; // es una clase de bootstrap modificar 5-20
+            cardDiv.innerHTML = `
+              <div class="producto">
+                <img src="${product.thumbnail}" class="img" alt="${product.title}" width="250">
+                <div class="detalle">
+                  <h5 class="titulo_producto">${product.title}</h5>
+                  <p class="precio_producto">Precio: $${product.price}</p>
+                  <button class="compra">Comprar</button>
+                </div>
+              </div>
+            `;
+            // Agregar evento al botón "Agregar"
+            const botonAgregar = cardDiv.querySelector("button");
+            botonAgregar.addEventListener("click", () => 
+              {
+              agregarAlCartito(product);
+            });
+  
+            // Añadir la card al contenedor
+            container.appendChild(cardDiv);
+          });
+        })
+        .catch((error) => console.error("Error", error));
+    }
+  
+    // Función para agregar al carrito usando localStorage
+    function agregarAlCartito(product) 
+    {
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
+      //agregar otro producto o el primero
+      cart.push(product);      
+      localStorage.setItem("cart", JSON.stringify(cart));
+      alert(`${product.title} ha sido agregado al carrito!`);
+    }
+  
+    // Carga inicial de productos
+    fetchProductos();
+  });
